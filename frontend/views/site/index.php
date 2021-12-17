15d55bed703dc628ccd9ea3e1f1ef0a5f6ccf36b
Mo'minjon <mominjonsamatov02@gmail.com>
2021-12-17 23:52:47 +0500

    frontend o'zgartirildi

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -35,7 +35,7 @@ $this->title = 'Yozyovon Online Bozor';
         <div class="hero-carousel__slide">
           <img src="img/home/hero-slide1.png" alt="" class="img-fluid">
           <a href="#" class="hero-carousel__slideOverlay">
-            <h3>Wireless Headphoneeeeeeeeeee</h3>
+            <h3>Wireless Headphonkdfkjhkvjkdjfkdjgdkkdk</h3>
             <p>Accessories Item</p>
           </a>
         </div>
